afa40ee54d1b8a61ecb6cafeab7bbde14feb22e7
Doug Kelly <dougkelly88@googlemail.com>
2015-11-13 20:46:16 +0000

    Work on choosing the correct trace to generate quantitative data from.

--- MATLAB/ViennaDataAutomated.m
+++ MATLAB/ViennaDataAutomated.m
@@ -8,6 +8,10 @@
 %by looking at mean frame intensity and setting a threshold above which the
 %whole frame is set to zero. 
 
+% import dimension metadata
+framespersecond = 5;
+umperpixel = 0.218;
+
 % import cut position from metadata - add offset??
 xoffset = -20;
 yoffset = -20;
@@ -111,16 +115,75 @@ for kpos = 1:num_kyms
     
 end
 
-% TO GET QUANTITATIVE DATA FROM KYMOGRAPHS
+%% TO GET QUANTITATIVE DATA FROM KYMOGRAPHS
+% for kpos = 1:num_kyms
+for kpos = 1:10
 % - trim kymograph - initially first 20 frames (=4 seconds)
+    kym_segment = squeeze(avg_kym_stack(144:164,:,kpos))';
 % - use canny edge filter to get binary image of membrane front movement
-% - choose the highest edge to represent the membrane front
-% - extract t,y coordinates of this edge
+    filt_kym = edge(kym_segment, 'canny');
+% - choose the highest edge to represent the membrane front, extract t, d
+% co-ordinates for this edge...
+    t = []; d = [];
+    for tind = 1:size(filt,1)
+        point_sel = false;
+        
+        for dind = 1:size(filt,2)
+            
+           if (not(point_sel)) && ( filt(tind, dind) > 0 )
+               point_sel = true;
+               filt_kym(tind, dind) = 1;
+           else
+               filt_kym(tind, dind) = 0;
+           end
+           
+        end
+        
+    end
+    
+    figure;
+    subplot(1,4,1);
+    imagesc(kym_segment);
+    axis equal tight;
+    subplot(1,4,2);
+    imagesc(filt_kym); 
+    axis equal tight; 
+    colormap gray;
+    subplot(1,4,3);
+    l = bwlabel(filt_kym);
+    imagesc(l);
+    r = regionprops(filt_kym);
+    % Sort regions by y position (centroid)
+    dpos = [];
+    for i = 1:length(r)
+        dpos = [dpos; r(i).Centroid(2)];
+    end
+    [dpos, I] = sort(dpos);
+    % Then check the time extent of the trace for each of these in order;
+    % the first one that is found that extends more than 80% of the trace
+    % is considered to be the right one
+    found = false;
+    i = 1;
+    while (found == false)
+        
+        if (r(I(i)).BoundingBox(3) > size(filt_kym,2)*.8)
+            found = true;
+        else
+            ++i;
+        end
+    end
+    correct_membrane = (l == I(i));
+    subplot(1,4,4);
+    imagesc(correct_membrane);
+        
+
 % - fit curve to this data (first order is a straight line, goodness of fit
 % tells us how linear motion actually is, discuss with supervisors what
 % alternative models to fit)
 % - convert to um/second
 
+
+end
 % OUTPUT FOR SUPERVISORS:
 % 
 
