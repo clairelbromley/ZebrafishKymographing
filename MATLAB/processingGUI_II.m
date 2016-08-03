a7cd01b0f9984416ecba16ef572df569a97daeea
d.kelly <d.kelly@base4.co.uk>
2016-08-03 06:41:14 +0100

    Sort mechanics of callbacks for panels Apical Surface Finder, Kymograph Positioning, Timing, Processing

--- MATLAB/processingGUI_II.m
+++ MATLAB/processingGUI_II.m
@@ -22,7 +22,7 @@ function varargout = processingGUI_II(varargin)
 
 % Edit the above text to modify the response to help processingGUI_II
 
-% Last Modified by GUIDE v2.5 12-Jul-2016 22:33:02
+% Last Modified by GUIDE v2.5 03-Aug-2016 06:35:55
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -54,6 +54,8 @@ function processingGUI_II_OpeningFcn(hObject, eventdata, handles, varargin)
 
 % Choose default command line output for processingGUI_II
 handles.output = hObject;
+
+handles.userOptions = UserOptions();
 
 % Update handles structure
 guidata(hObject, handles);
@@ -120,18 +122,18 @@ end
 
 
 
-function edit3_Callback(hObject, eventdata, handles)
-% hObject    handle to edit3 (see GCBO)
+function outputFolderText_Callback(hObject, eventdata, handles)
+% hObject    handle to outputFolderText (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of edit3 as text
-%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+% Hints: get(hObject,'String') returns contents of outputFolderText as text
+%        str2double(get(hObject,'String')) returns contents of outputFolderText as a double
 
 
 % --- Executes during object creation, after setting all properties.
-function edit3_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit3 (see GCBO)
+function outputFolderText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to outputFolderText (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
@@ -161,9 +163,11 @@ function timeBeforeCutText_Callback(hObject, eventdata, handles)
 % hObject    handle to timeBeforeCutText (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hints: get(hObject,'String') returns contents of timeBeforeCutText as text
-%        str2double(get(hObject,'String')) returns contents of timeBeforeCutText as a double
+handles.userOptions.timeBeforeCut = str2double(get(hObject, 'String'));
+
+guidata(hObject, handles);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -184,9 +188,11 @@ function timeAfterCutText_Callback(hObject, eventdata, handles)
 % hObject    handle to timeAfterCutText (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hints: get(hObject,'String') returns contents of timeAfterCutText as text
-%        str2double(get(hObject,'String')) returns contents of timeAfterCutText as a double
+handles.userOptions.timeAfterCut = str2double(get(hObject, 'String'));
+
+guidata(hObject, handles);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -207,10 +213,11 @@ function quantitativeAnalysisTimeText_Callback(hObject, eventdata, handles)
 % hObject    handle to quantitativeAnalysisTimeText (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hints: get(hObject,'String') returns contents of quantitativeAnalysisTimeText as text
-%        str2double(get(hObject,'String')) returns contents of quantitativeAnalysisTimeText as a double
+handles.userOptions.quantAnalysisTime = str2double(get(hObject, 'String'));
 
+guidata(hObject, handles);
 
 % --- Executes during object creation, after setting all properties.
 function quantitativeAnalysisTimeText_CreateFcn(hObject, eventdata, handles)
@@ -231,8 +238,19 @@ function kymPositioningText_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of kymPositioningText as text
-%        str2double(get(hObject,'String')) returns contents of kymPositioningText as a double
+handles = guidata(gcf);
+
+if get(handles.fixedNumberRadio, 'Value')
+    
+    handles.userOptions.number_kym  = str2double(get(hObect, 'String'));
+    
+else
+    
+    handles.userOptions.kymSpacingUm = str2double(get(hObect, 'String'));
+    
+end
+
+guidata(hObject, handles);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -254,8 +272,13 @@ function kymWidthText_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of kymWidthText as text
-%        str2double(get(hObject,'String')) returns contents of kymWidthText as a double
+handles = guidata(gcf);
+
+handles.userOptions.kym_width = str2double(get(hObject, 'String'));
+
+guidata(hObject, handles);
+
+
 
 
 % --- Executes during object creation, after setting all properties.
@@ -277,8 +300,11 @@ function kymLengthText_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of kymLengthText as text
-%        str2double(get(hObject,'String')) returns contents of kymLengthText as a double
+handles = guidata(gcf);
+
+handles.userOptions.kym_length = str2double(get(hObject, 'String'));
+
+guidata(hObject, handles);
 
 
 % --- Executes during object creation, after setting all properties.
@@ -299,8 +325,12 @@ function showOverlaysCheck_Callback(hObject, eventdata, handles)
 % hObject    handle to showOverlaysCheck (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hint: get(hObject,'Value') returns toggle state of showOverlaysCheck
+handles.userOptions.showKymographOverlapOverlay = get(hObject, 'Value');
+
+guidata(hObject, handles);
+
 
 
 % --- Executes on button press in preProcessCheck.
@@ -308,8 +338,11 @@ function preProcessCheck_Callback(hObject, eventdata, handles)
 % hObject    handle to preProcessCheck (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hint: get(hObject,'Value') returns toggle state of preProcessCheck
+handles.userOptions.preProcess = get(hObject, 'Value');
+
+guidata(hObject, handles);
 
 
 % --- Executes on button press in loadPreprocessedCheck.
@@ -317,8 +350,11 @@ function loadPreprocessedCheck_Callback(hObject, eventdata, handles)
 % hObject    handle to loadPreprocessedCheck (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hint: get(hObject,'Value') returns toggle state of loadPreprocessedCheck
+handles.userOptions.loadPreprocessedImages = get(hObject, 'Value');
+
+guidata(hObject, handles);
 
 
 % --- Executes on button press in savePreprocessedCheck.
@@ -326,23 +362,25 @@ function savePreprocessedCheck_Callback(hObject, eventdata, handles)
 % hObject    handle to savePreprocessedCheck (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hint: get(hObject,'Value') returns toggle state of savePreprocessedCheck
+handles.userOptions.savePreprocessed = get(hObject, 'Value');
+
+guidata(hObject, handles);
 
 
-
-function edit10_Callback(hObject, eventdata, handles)
-% hObject    handle to edit10 (see GCBO)
+function medianFilterText_Callback(hObject, eventdata, handles)
+% hObject    handle to medianFilterText (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of edit10 as text
-%        str2double(get(hObject,'String')) returns contents of edit10 as a double
+% Hints: get(hObject,'String') returns contents of medianFilterText as text
+%        str2double(get(hObject,'String')) returns contents of medianFilterText as a double
 
 
 % --- Executes during object creation, after setting all properties.
-function edit10_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit10 (see GCBO)
+function medianFilterText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to medianFilterText (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
@@ -353,13 +391,16 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
-% --- Executes on button press in checkbox5.
-function checkbox5_Callback(hObject, eventdata, handles)
-% hObject    handle to checkbox5 (see GCBO)
+% --- Executes on button press in removeCutFramesCheck.
+function removeCutFramesCheck_Callback(hObject, eventdata, handles)
+% hObject    handle to removeCutFramesCheck (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hint: get(hObject,'Value') returns toggle state of checkbox5
+handles.userOptions.removeCutFrames = get(hObject, 'Value');
+
+guidata(hObject, handles);
 
 
 % --- Executes on button press in apicalSurfaceFinderLoadCheck.
@@ -367,8 +408,12 @@ function apicalSurfaceFinderLoadCheck_Callback(hObject, eventdata, handles)
 % hObject    handle to apicalSurfaceFinderLoadCheck (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
 
-% Hint: get(hObject,'Value') returns toggle state of apicalSurfaceFinderLoadCheck
+handles.userOptions.usePreviouslySavedApicalSurfacePos = get(hObject, 'Value');
+
+guidata(hObject, handles);
+
 
 
 % --- Executes on selection change in outputPopup.
@@ -392,3 +437,211 @@ function outputPopup_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+
+function scalebarLengthText_Callback(hObject, eventdata, handles)
+% hObject    handle to scalebarLengthText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of scalebarLengthText as text
+%        str2double(get(hObject,'String')) returns contents of scalebarLengthText as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function scalebarLengthText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to scalebarLengthText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in checkbox7.
+function checkbox7_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox7
+
+
+% --- Executes on button press in shortCutCheck.
+function shortCutCheck_Callback(hObject, eventdata, handles)
+% hObject    handle to shortCutCheck (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
+
+handles.userOptions.flip90DegForShortCuts = get(hObject, 'Value');
+
+guidata(hObject, handles);
+
+
+% --- Executes on button press in forceRangesCheck.
+function forceRangesCheck_Callback(hObject, eventdata, handles)
+% hObject    handle to forceRangesCheck (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of forceRangesCheck
+
+
+
+function forceMinPosText_Callback(hObject, eventdata, handles)
+% hObject    handle to forceMinPosText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of forceMinPosText as text
+%        str2double(get(hObject,'String')) returns contents of forceMinPosText as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function forceMinPosText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to forceMinPosText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function forceMaxPosText_Callback(hObject, eventdata, handles)
+% hObject    handle to forceMaxPosText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of forceMaxPosText as text
+%        str2double(get(hObject,'String')) returns contents of forceMaxPosText as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function forceMaxPosText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to forceMaxPosText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function forceMinSpeedText_Callback(hObject, eventdata, handles)
+% hObject    handle to forceMinSpeedText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of forceMinSpeedText as text
+%        str2double(get(hObject,'String')) returns contents of forceMinSpeedText as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function forceMinSpeedText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to forceMinSpeedText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function forceMaxSpeedText_Callback(hObject, eventdata, handles)
+% hObject    handle to forceMaxSpeedText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of forceMaxSpeedText as text
+%        str2double(get(hObject,'String')) returns contents of forceMaxSpeedText as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function forceMaxSpeedText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to forceMaxSpeedText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in goButton.
+function goButton_Callback(hObject, eventdata, handles)
+% hObject    handle to goButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+handles = guidata(gcf);
+
+if get(handles.upAndDownRadio, 'Value')
+    
+%     kymographUpDownWrapper(
+    % refactor kymographUpDownWrapper to take a UserOptions object as an
+    % argument
+    
+else
+    kymographBase(this.userOptions);
+end
+
+
+% --- Executes when selected object is changed in apicalSurfaceFinderPanel.
+function apicalSurfaceFinderPanel_SelectionChangeFcn(hObject, eventdata, handles)
+% hObject    handle to the selected object in apicalSurfaceFinderPanel 
+% eventdata  structure with the following fields (see UIBUTTONGROUP)
+%	EventName: string 'SelectionChanged' (read only)
+%	OldValue: handle of the previously selected object or empty if none was selected
+%	NewValue: handle of the currently selected object
+% handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
+
+if get(handles.apicalSurfaceFinderOffRadio, 'Value')
+    handles.userOptions.manualOrAutoApicalSurfaceFinder = 'off';
+elseif get(handles.apicalSurfaceFinderManualRadio, 'Value')
+    handles.userOptions.manualOrAutoApicalSurfaceFinder = 'manual';
+elseif get(handles.apicalSurfaceFinderAutoRadio, 'Value')
+    handles.userOptions.manualOrAutoApicalSurfaceFinder = 'auto';
+end
+        
+guidata(hObject, handles);
+
+
+% --- Executes when selected object is changed in kymographPositioningPanel.
+function kymographPositioningPanel_SelectionChangeFcn(hObject, eventdata, handles)
+% hObject    handle to the selected object in kymographPositioningPanel 
+% eventdata  structure with the following fields (see UIBUTTONGROUP)
+%	EventName: string 'SelectionChanged' (read only)
+%	OldValue: handle of the previously selected object or empty if none was selected
+%	NewValue: handle of the currently selected object
+% handles    structure with handles and user data (see GUIDATA)
+handles = guidata(gcf);
+
+if get(handles.fixedSpacingRadio, 'Value')
+    set(handles.kymPositionLabel, 'String', 'Spacing (um):');
+    handles.userOptions.fixedNumberOrFixedSpacing = true;
+    set(handles.kymPositioningText, 'String', num2str(handles.userOptions.kymSpacingUm));
+elseif get(handles.fixedNumberRadio, 'Value')
+    set(handles.kymPositionLabel, 'String', '# of kymos:');
+    handles.userOptions.fixedNumberOrFixedSpacing = false;
+    set(handles.kymPositioningText, 'String', num2str(handles.userOptions.number_kym));
+end
+        
+guidata(hObject, handles);
