22898ca23c87b2dc0d88f09e3238009fb5de872e
d.kelly <d.kelly@base4.co.uk>
2016-09-15 06:41:58 +0100

    Bug fix, stray code from copy paste.

--- MATLAB/UserOptions.m
+++ MATLAB/UserOptions.m
@@ -14,7 +14,7 @@ classdef UserOptions
          timeBeforeCut = 5;                  % Time in seconds before cut for kymograph to start.                                        Default = 5
          timeAfterCut = 10;                  % Time in seconds after cut for kymograph to end.                                           Default = 10
          quantAnalysisTime = 4;              % Time in seconds over which to fit quantitative data model. 
-
+         
          kym_width = 5;                      % Width of region kymograph calculated over, pix. Must be odd.                              Default = 9
          kym_length = 50;                    % Length of region kymograph calculated over, pix.                                          Default = 50
 
@@ -24,7 +24,7 @@ classdef UserOptions
          saveFirstFrameFigure = true;        % Save first figure?                                                                        Default = true
          firstFigureTitleAppend = '' ;       % Text to append to the title of the first figure.                                          Default = ''
          saveCutPositioningFigs = true;     % Toggle saving of helper images for checking cut positioning.                              Default = false
-         userOptions.removeCutFrames = 'auto';             % Switch removal of scattered light frames between 'off', 'auto', 'manual' and 'previous manual'.              Default = 'auto'
+         removeCutFrames = 'auto';             % Switch removal of scattered light frames between 'off', 'auto', 'manual' and 'previous manual'.              Default = 'auto'
          figHandle = figure;                 % Allow figures to be rendered in a single window. 
          savePreprocessed = true;            % Save stack of images following preprocessing with cut position information.               Default = true
          avgOrMax = 1;                       % Choose between averaging (1) or taking max over (2) the kym_width per kym.                Default = 1
