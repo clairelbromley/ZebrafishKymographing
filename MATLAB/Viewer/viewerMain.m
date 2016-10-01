2edc074858528aaa0e0b4c93589664a8e9b75625
d.kelly <d.kelly@base4.co.uk>
2016-10-01 16:17:06 +0100

    FIxed new error parsing metadata when no edge found.

--- MATLAB/Viewer/viewerMain.m
+++ MATLAB/Viewer/viewerMain.m
@@ -427,7 +427,7 @@ try
                            qcLabel = 'no edge';
                            handles = genericInclude(handles, qcLabel, direction{1}, pos);
                        else
-                           if ((sum(round(1000*handles.poss{strcmp(directions, direction{1})})/1000 == round(1000*pos)/1000) > 0))  % nonsense
+                           if ((sum(round(1000*handles.positionsAlongLine)/1000 == round(1000*pos)/1000) > 0))  % nonsense
                                qcLabel = 'not QCd';
                                handles = genericInclude(handles, qcLabel, direction{1}, pos);
                            else
