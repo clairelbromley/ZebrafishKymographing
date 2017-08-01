f1795abc5eca4aa0950c990208fe6de699e17902
Doug Kelly <dougkelly88@googlemail.com>
2017-08-01 22:22:54 +0100

    Fix edge visualisation bug

--- MATLAB/Krox20Analysis/show_edges.m
+++ MATLAB/Krox20Analysis/show_edges.m
@@ -1,33 +1,40 @@
 function show_edges(controls, data)
 
-    if get(controls.hshowchk, 'Value')
-
-        % get the current z value and check whether it's in the list
-        current_z_ind = get(controls.hzsl, 'Value');
-        if isfield(data, 'top_slice_index')
-            z_ind_to_micron_depth = double(data.ome_meta.getPixelsPhysicalSizeZ(0).value(ome.units.UNITS.MICROM));
-            slices_relative_to_top = round(data.z_offsets/ z_ind_to_micron_depth);
-            z = round((data.top_slice_index - current_z_ind) * z_ind_to_micron_depth);
-            if ~any(current_z_ind == (data.top_slice_index - slices_relative_to_top))
-                return;
-            end
-        else
+    % get the current z value and check whether it's in the list
+    current_z_ind = get(controls.hzsl, 'Value');
+    if isfield(data, 'top_slice_index')
+        z_ind_to_micron_depth = double(data.ome_meta.getPixelsPhysicalSizeZ(0).value(ome.units.UNITS.MICROM));
+        slices_relative_to_top = round(data.z_offsets/ z_ind_to_micron_depth);
+        z = round((data.top_slice_index - current_z_ind) * z_ind_to_micron_depth);
+        if ~any(current_z_ind == (data.top_slice_index - slices_relative_to_top))
             return;
         end
+    else
+        return;
+    end
 
-        % get the current timepoint
-        t = data.timepoint;
+    % get the current timepoint
+    t = data.timepoint;
+
+    if get(controls.hshowchk, 'Value')
 
         % access the edges structure and draw the edges
-        edgs = {'hlL', 'hlM', 'hlR'};
+        edgs = {'L', 'M', 'R'};
         if ~isempty(data.edges)
             ts = [data.edges.timepoint];
             zs = [data.edges.z];
             if any(ts == t)
                 if any(zs(ts == t) == z)
                     for edg = edgs
-                        if ~isempty(data.edges(zs(ts == t) == z).(edg{1}))
-                            set(data.edges(zs(ts == t) == z).(edg{1}), 'Visible', 'on');
+                        if ~isempty(data.edges(zs(ts == t) == z).(['hl' edg{1}]))
+                            if isgraphics(data.edges(zs(ts == t) == z).(['hl' edg{1}]))
+                                set(data.edges(zs(ts == t) == z).(['hl' edg{1}]), 'Visible', 'on');
+                            else
+                                data.edges(zs(ts == t) == z).(['hl' edg{1}]) = ...
+                                    line(data.edges(zs(ts == t) == z).(edg{1})(:,1), ...
+                                    data.edges(zs(ts == t) == z).(edg{1})(:,2), ...
+                                    'Color', 'r', 'Visible', 'on');
+                            end
                         end
                     end
                 end
@@ -35,16 +42,8 @@ function show_edges(controls, data)
         end
         
     else    
-        if ~isempty(data.edges)
-            edgelines = {'hlL', 'hlR', 'hlM'};
-            for eidx = 1:length(data.edges)
-                for elidx = 1:length(edgelines)
-                    if ~isempty(data.edges(eidx).(edgelines{elidx}))
-                        set(data.edges(eidx).(edgelines{elidx}), 'Visible', 'off');
-                    end
-                end
-            end
-        end
+        kids = get(controls.hax, 'Children');
+        delete(kids(strcmp(get(kids, 'Type'), 'line')));
     end
     
     setappdata(controls.hfig, 'data', data);
