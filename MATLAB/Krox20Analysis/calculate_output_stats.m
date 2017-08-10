710d90a80179ff962b92164c17874158a0c84929
d.kelly <d.kelly@base4.co.uk>
2017-08-10 19:30:48 +0100

    Fixed first tranche of bugs with calculation of sinuosity index. Further testing required.

--- MATLAB/Krox20Analysis/calculate_output_stats.m
+++ MATLAB/Krox20Analysis/calculate_output_stats.m
@@ -1,19 +1,24 @@
 function calculate_output_stats(data)
 
     % filter edges structure for the correct timepoint
-    edges = data.edges(data.edges.timepoint == data.timepoint);
+    edges = data.edges([data.edges.timepoint] == data.timepoint);
     
     % cycle through z positions
     for z = [edges.z]
 
-        edge = edges(edges.z == z);
+        edge = edges([edges.z] == z);
         
         % midline sinuoisity
         theta = edge.tissueRotation;
         rotMatrix = [cos(theta) -sin(theta); sin(theta) cos(theta)];
-        rotated_midline = rotMatrix * edge.M;
-        rotated_midline(:, rotated_midline(:, 2) > edge.rhombomereLimits(2)) = [];
-        rotated_midline(:, rotated_midline(:, 2) < edge.rhombomereLimits(1)) = [];
+        c = [double(data.ome_meta.getPixelsSizeY(0).getNumberValue()), ...
+            double(data.ome_meta.getPixelsSizeX(0).getNumberValue())]/2;
+        cc = repmat(c, size(edge.M, 1), 1);
+        rotated_midline = (rotMatrix * (edge.M - cc)')' + cc;
+        msk1 = rotated_midline(:, 2) > edge.rhombomereLimits(2);
+        msk2 = rotated_midline(:, 2) < edge.rhombomereLimits(1);
+        msk = msk1 | msk2;
+        rotated_midline(msk, :) = [];
         straight_length = sqrt((rotated_midline(end, 1) - rotated_midline(1, 1))^2 + ...
             (rotated_midline(end, 2) - rotated_midline(1, 2))^2 );
         manual_length = 0;
