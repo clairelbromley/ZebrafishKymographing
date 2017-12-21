f0d91dc49377a8a0b7d1439a12e58e4b9040d2e3
Doug Kelly <dougkelly88@googlemail.com>
2017-12-21 21:20:36 +0000

    WiP new AP length calc

--- MATLAB/Krox20Analysis/calc_ap_lengths.m
+++ MATLAB/Krox20Analysis/calc_ap_lengths.m
@@ -1,14 +1,23 @@
-function ap_lengths = calc_ap_lengths(data, edge)
+function ap_lengths = calc_ap_lengths(data, xedge)
 
 
     rhs = [4, 5, 6];
     pix_to_micron = double(data.ome_meta.getPixelsPhysicalSizeX(0).value(ome.units.UNITS.MICROM));
     
     for rh = rhs
-        if sum(edge.rhombomereLimits) > 0
-            ap_lengths.(['Rh' num2str(rh)]) = abs(edge.rhombomereLimits(rh-min(rhs)+2) - ...
-                edge.rhombomereLimits(rh-min(rhs)+1)) * pix_to_micron;
-            ap_lengths.AllRh = abs(edge.rhombomereLimits(end) - edge.rhombomereLimits(1)) * pix_to_micron;
+        if sum(xedge.rhombomereLimits) > 0
+            if strcmp(data.AP_axis_method, 'Rhombomeres')
+                ap_lengths.(['Rh' num2str(rh)]) = abs(xedge.rhombomereLimits(rh-min(rhs)+2) - ...
+                    xedge.rhombomereLimits(rh-min(rhs)+1)) * pix_to_micron;
+                ap_lengths.AllRh = abs(xedge.rhombomereLimits(end) - xedge.rhombomereLimits(1)) * pix_to_micron;
+            else
+                disp('nonsense');
+                %CONSIDER WHETHER TO DO AP LENGTHS FOR ALL RH IN THIS CASE.
+                %IF SO, NEED TO BE CAREFUL TO DIFFERENTIATE BETWEEN LOWER
+                %AND UPPER EDGES OF RHOMBOMERES - SAY TAKE SEGMENT WITHIN X
+                %PIXELS OF UPPER (HIGH Y) AND LOWER (LOW Y) EDGES
+                %RESPECTIVELY. 
+            end
         else
             ap_lengths.(['Rh' num2str(rh)]) = NaN;
             ap_lengths.AllRh = NaN;
